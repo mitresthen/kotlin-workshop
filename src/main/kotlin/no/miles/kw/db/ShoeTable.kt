4f801bd8ffc8d15264367d642592ee403dcf066f
Yngve Aas <yngve.aas@miles.no>
2022-02-21 17:21:08 +0100

    La til auto id pk

--- src/main/kotlin/no/miles/kw/db/ShoeTable.kt
+++ src/main/kotlin/no/miles/kw/db/ShoeTable.kt
@@ -3,8 +3,9 @@ package no.miles.kw.db
 import org.jetbrains.exposed.sql.Table
 
 object ShoeTable : Table("SHOETABLE") {
+    val id = integer("id").autoIncrement()
     val name = varchar("NAME", 255)
     val brand = varchar("BRAND", 255)
 
-    override val primaryKey: PrimaryKey = PrimaryKey(name, name = "${tableName}_pk")
+    override val primaryKey: PrimaryKey = PrimaryKey(id, name = "${tableName}_pk")
 }
